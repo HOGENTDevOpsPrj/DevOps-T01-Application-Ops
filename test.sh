8872aab6e337657f26190814e2b11856609470b8
JelleJacobs <58846357+jellejacobs@users.noreply.github.com>
2023-12-04 22:03:30 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,15 +4,13 @@ expected_directory="/gitrepo"
 
 current_directory="$(pwd)"
 
-cd ~
-$SHELL
-sudo rm -r -f /gitrepo
+
 if [ -n "$(docker ps -f "name=db" -f "status=running" -q )" ] ; then docker rm -f db
 fi
 
 if [ -n "$(docker ps -f "name=web" -f "status=running" -q )" ] ; then docker rm -f web
 fi
-git clone https://github.com/HOGENTDevOpsPrj/DevOps-T01-Application.git /gitrepo
+
 if [ "$current_directory" != "$expected_directory" ]; then
   echo 'Go to the GIT directory'
   cd /gitrepo 
